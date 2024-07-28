6f57cc5d576a8e9fdaf39b41ed75f8ca74365dd9
vinccnt <vincent@lexer.markets>
2024-07-28 14:50:26 +0900

    update

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -8,9 +8,9 @@ import { Deployer } from "@matterlabs/hardhat-zksync";
 // It will deploy a CrowdfundingCampaign contract to selected network
 // `parseEther` converts ether to wei, and `.toString()` ensures serialization compatibility.
 export default async function () {
-  // const contractArtifactName = "Coffee";
-  // const constructorArguments = [)];
-  // await deployContract(contractArtifactName, constructorArguments);
+  const contractArtifactName = "NFT";
+  const constructorArguments = ["ZK_QUEST", "ZKQ", "baseUri"];
+  await deployContract(contractArtifactName, constructorArguments);
 
   // const contractArtifactName = "ZkNFT";
   // const constructorArguments = ["0xfdFd2304af41bAf61092d511BfD4caa6cAEC8D86"];
@@ -18,8 +18,8 @@ export default async function () {
 
 
 
-  const contractArtifactName = "ERC20Upgrade";
-  const constructorArguments = [];
-  await UpgradeContract(contractArtifactName, constructorArguments);
+  // const contractArtifactName = "ERC20Upgrade";
+  // const constructorArguments = [];
+  // await UpgradeContract(contractArtifactName, constructorArguments);
 }
 
